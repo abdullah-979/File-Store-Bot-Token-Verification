66b8e164986b1153397e29b3bde41b95798bdcf0
Abdullah <78228041+abdullah-979@users.noreply.github.com>
2024-07-02 07:35:32 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,13 +10,13 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>Join \n @StarFlixTamil \n @TamilLinksOfficial </b>",
+            text = f"<b>○ ᴏᴡɴᴇʀ : <a href='tg://user?id={OWNER_ID}'>Tamil Feeds</a>\n○ ᴍʏ ᴜᴘᴅᴀᴛᴇs : <a href='https://t.me/TamilLinksOfficial'>TamilLinksOfficial</a>\n○ ᴍᴏᴠɪᴇs ᴜᴘᴅᴀᴛᴇs : <a href='https://t.me/TamilFilmsOfficial'>TamilFilms</a>\n○ ᴏᴜʀ ᴄᴏᴍᴍᴜɴɪᴛʏ : <a href='https://t.me/TamilLinksOfficialTeam'>Connect With TLO</a></b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
                     [
-                    InlineKeyboardButton("Close", callback_data = "close"),
-                    InlineKeyboardButton('Premium', url='https://t.me/ChatBoxOfficialBot')
+                    InlineKeyboardButton("• Close", callback_data = "close"),
+                    InlineKeyboardButton('• Premium', url='https://t.me/ChatBoxOfficialBot')
                     ]
                 ]
             )
